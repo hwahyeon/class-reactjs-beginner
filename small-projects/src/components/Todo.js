c56a2ca47b9970b5411d1982b3d6ac0c921f1219
hwahyeon <hwahyeon@knou.ac.kr>
2022-09-07 19:36:52 +0900

    Add the funciton to remove ToDoList

--- small-projects/src/components/Todo.js
+++ small-projects/src/components/Todo.js
@@ -9,14 +9,30 @@ function App() {
         event.preventDefault();
         if (toDo === "") {
             return;
+        } else if (toDos.includes(toDo)) {
+            alert("This 'To Do' already exists");
+            return;
         }
+
         setToDos((currentArray) => [toDo, ...currentArray]);
         setToDo("");
     };
+    const deleteAll = (event) => {
+        event.preventDefault();
+        setToDos((currentArray) => []);
+    };
+    const deleteTodo = (event) => {
+        const targetItem = event.target.id;
+        setToDos((currentArray) =>
+            currentArray.filter((item) => {
+                return item !== targetItem;
+            })
+        );
+    };
     return (
         <div>
-            <h1>My To Dos ({toDos.length})</h1>
-            <form onSubmit={onSubmit}>
+            <h1 className={styles.head}>My To Dos ({toDos.length})</h1>
+            <form onSubmit={onSubmit} className={styles.form}>
                 <input
                     value={toDo}
                     onChange={onChange}
@@ -24,11 +40,21 @@ function App() {
                     placeholder="Wirte your to do"
                 />
                 <button>Add To Do</button>
+                <button onClick={deleteAll}>Delete All</button>
             </form>
             <hr />
-            <ul>
+            <ul className={styles.ul}>
                 {toDos.map((item, index) => (
-                    <li key={index}>{item}</li>
+                    <li key={index}>
+                        {item}
+                        <button
+                            className={styles.btn_todo}
+                            id={item}
+                            onClick={deleteTodo}
+                        >
+                            ✓
+                        </button>
+                    </li>
                 ))}
             </ul>
         </div>
